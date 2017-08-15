417371f08d3e36403ca684e9894aaf35af5c293e
sunrise81 <luoruijunblue@hotmail.com>
2017-08-15 13:42:30 +0800

    把删除改成自行绑事件

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,19 @@ class PostsController < ApplicationController
      before_action :authenticate_user!, :only => [:create, :destroy]
 
      def index
-       @posts = Post.order("id DESC").all # 新帖文放前面
+       @posts = Post.order("id DESC").all .limit(20)# 新帖文放前面
+
+        if params[:max_id]
+          @posts = @posts.where( "id < ?", params[:max_id])
+        end
+
+        respond_to do |format|
+          format.html  # 如果客户端要求 HTML，则回传 index.html.erb
+          format.js    # 如果客户端要求 JavaScript，回传 index.js.erb
+        end
      end
+
+
 
      def create
        @post = Post.new(post_params)
@@ -15,7 +26,7 @@ class PostsController < ApplicationController
      def destroy
        @post = current_user.posts.find(params[:id]) # 只能删除自己的贴文
        @post.destroy
-
+       render :json => { :id => @post.id }
      end
 
     def like
