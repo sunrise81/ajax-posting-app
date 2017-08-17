a67d6fcab037b0da7bae8c843cb6e54f6fde799f
sunrise81 <luoruijunblue@hotmail.com>
2017-08-17 13:42:06 +0800

    Ajax动画效果

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,12 @@
 class Post < ApplicationRecord
    validates_presence_of :content
    belongs_to :user
-
+   belongs_to :category, :optional => true
    has_many :likes, :dependent => :destroy
    has_many :liked_users, :through => :likes, :source => :user
    has_many :collections, :dependent => :destroy
    has_many :collected_users, :through => :collections, :source => :user
+
 
    def find_like(user)
      self.likes.where( :user_id => user.id ).first
